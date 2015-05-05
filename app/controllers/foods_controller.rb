cd490d8b6d8e680d32b36bd8b4dea29d5b652c36
JJLim <lim.jess9@gmail.com>
2015-05-05 14:17:48 -0400

    Create foods controller

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,41 @@
+class FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to foods_url
+    else
+      render :new
+    end
+  end
+
+  def show
+    @food = Food.find(params[:id])
+  end
+
+  def edit
+    @food = Food.find(params[:id])
+  end
+
+  def update
+    @food = Food.find(params[:id])
+    if @food.update_attributes(food_params)
+      redirect_to food_path(@food)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def food_params
+    params.require(:food).permit(:name)
+  end
+
+end
