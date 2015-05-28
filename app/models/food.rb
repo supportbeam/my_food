13dd7314883f0d7b275e31e38d6d39f9bf8ac9a4
JJLim <lim.jess9@gmail.com>
2015-05-28 14:33:12 -0400

    Create list model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,4 @@
 class Food < ActiveRecord::Base
   serialize :categories, Array
+  belongs_to :list
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150506192019) do
+ActiveRecord::Schema.define(version: 20150527001346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20150506192019) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.text     "categories",   default: [],              array: true
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "title"
+    t.boolean  "checked"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
