32ceb8f9731793819f12935d5e8d0cbd65a93b0d
JJLim <lim.jess9@gmail.com>
2015-05-28 15:13:32 -0400

    Generate users controller and configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   resources :foods, except: [:destroy]
   resources :lists, except: [:destroy]
+  resources :users, except: [:destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
